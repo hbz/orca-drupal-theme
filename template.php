0eb7a855fe58e26eab44dfbba355644a3f6ee41a
Andres Quast <quast@hbz-nrw.de>
2022-02-02 14:41:31 +0100

    remove fonts from google api from code

--- template.php
+++ template.php
@@ -15,12 +15,15 @@
   */
 
   // Droid font family: a complete family with serif and sans
-    drupal_add_css('https://fonts.googleapis.com/css?family=Droid+Serif|Droid+Sans:400,700&subset=latin,latin-ext', array('type' => 'external'));
+  //  drupal_add_css('https://fonts.googleapis.com/css?family=Droid+Serif|Droid+Sans:400,700&subset=latin,latin-ext', array('type' => 'external'));
   // Signika font: only serif 
-   drupal_add_css('https://fonts.googleapis.com/css?family=Signika:400,600,700,300&subset=latin,latin-ext', array('type' => 'external'));
+  // drupal_add_css('https://fonts.googleapis.com/css?family=Signika:400,600,700,300&subset=latin,latin-ext', array('type' => 'external'));
   // PT font family: a complete family with serif and sans
-  drupal_add_css('https://fonts.googleapis.com/css?family=PT+Serif|PT+Sans|PT+Sans+Narrow|PT+Sans+Caption&subset=latin,latin-ext', array('type' => 'external'));
+  // drupal_add_css('https://fonts.googleapis.com/css?family=PT+Serif|PT+Sans|PT+Sans+Narrow|PT+Sans+Caption&subset=latin,latin-ext', array('type' => 'external'));
 
+  // Add Font-Open-Sans as replacement for droid
+  drupal_add_css('css/font-open_sans.css');
+  
   // Add Font-Awesome
   drupal_add_css('css/font-awesome.min.css');
   
